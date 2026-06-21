61fed1cf4d16461ceadd194763b0c25e16ed2ff5
25-cs-005-Muhammad <25-cs-005@student.hitecuni.edu.pk>
2026-06-22 00:35:43 +0500

    Update Course.h

--- SOURCE/course/Course.h
+++ SOURCE/course/Course.h
@@ -5,30 +5,7 @@
 #include "../person/Faculty.h"
 using namespace std;
 
-class Course {
-private:
-    string courseCode;
-    string courseName;
-    int creditHours;
-    Faculty* instructor;
-    int maxCapacity;
-    int enrolledCount;
-public:
-    Course(string code, string name, int credit, Faculty* inst, int maxcap)
-        : courseCode(code), courseName(name), creditHours(credit),
-          instructor(inst), maxCapacity(maxcap), enrolledCount(0) {}
 
-    string getCourseCode() const { return courseCode; }
-    string getCourseName() const { return courseName; }
-    int getCreditHours() const { return creditHours; }
-    int getEnrolledCount() const { return enrolledCount; }
-    int getMaxCapacity() const { return maxCapacity; }
-
-    void setCourseName(string name) { courseName = name; }
-
-    bool operator==(const Course& other) const {
-        return courseCode == other.courseCode;
-    }
 
     void enrollStudent() {
         if (enrolledCount >= maxCapacity)
